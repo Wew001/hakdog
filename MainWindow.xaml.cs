86860e8fa064deb59bc12601b6122e7f671a1daa
Wew001 <156871440+Wew001@users.noreply.github.com>
2024-03-06 17:37:59 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void INVENTORY_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UP_KEY_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void UP_KEY_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Key == Key.W)
+
+            { UpKeyPressed = true; }
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
